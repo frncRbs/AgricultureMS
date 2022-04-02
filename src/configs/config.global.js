e725aaa02853129a6637ba00587ec2549e0033e8
France <shenlong0071@gmail.com>
2022-04-02 16:23:15 +0800

    Setup production

--- src/configs/config.global.js
+++ src/configs/config.global.js
@@ -16,7 +16,10 @@ module.exports = {
         methodOverride('X-HTTP-Method-Override') /* Google/GData */,
         methodOverride('X-Method-Override') /* IBM */,
         (req, res, next) => {
-            const allowedOrigins = ['http://localhost:3000'];
+            const allowedOrigins = [
+                'http://localhost:3000',
+                'https://ayala-agriculturist.netlify.app/',
+            ];
             const origin = req.headers.origin;
             if (allowedOrigins.includes(origin)) {
                 res.setHeader('Access-Control-Allow-Origin', origin);

2bd6247691f2fb25bbb9a927e5f4fb643c3d3af5
France <shenlong0071@gmail.com>
2022-04-05 18:41:43 +0800

    Update production setup

--- client/src/app/axiosClient.js
+++ client/src/app/axiosClient.js
@@ -5,8 +5,8 @@ const privateApi = axios.create();
 const publicApi = axios.create();
 
 const instanceConfig = (instance) => {
-    instance.defaults.baseURL = 'https://ayalaserver.herokuapp.com';
     // instance.defaults.withCredentials = true;
+    instance.defaults.baseURL = 'https://ayalaserver.herokuapp.com';
 };
 
 instanceConfig(publicApi); /* public HTTP request */
